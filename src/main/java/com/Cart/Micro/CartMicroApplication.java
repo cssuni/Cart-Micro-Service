5a3a058f52d056fe436f14d46a79a4bdcd9f2db4
sunil <cssunilkohli@gmail.com>
2025-03-09 19:40:19 +0530

    Initial Commit

---
+++ src/main/java/com/Cart/Micro/CartMicroApplication.java
@@ -0,0 +1,13 @@
+package com.Cart.Micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartMicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartMicroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Cart/Micro/CartMicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Cart.Micro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartMicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
