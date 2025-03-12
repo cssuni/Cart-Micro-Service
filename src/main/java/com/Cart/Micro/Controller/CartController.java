c7c733e4834adcbe54f656bc9b409f52f5c7f3d2
sunil <cssunilkohli@gmail.com>
2025-03-12 17:12:03 +0530

    Order Integration Completed

--- src/main/java/com/Cart/Micro/Controller/CartController.java
+++ src/main/java/com/Cart/Micro/Controller/CartController.java
@@ -26,25 +26,25 @@ public class CartController {
         return ResponseEntity.ok(new ApiResponse("Cart created successfully", cartService.initializeNewCart()));
     }
 
-    @GetMapping("get/{cartId}")
-    public ResponseEntity<ApiResponse> getCart(@PathVariable Long cartId){
+    @GetMapping("get")
+    public ResponseEntity<Cart> getCart(@RequestParam Long cartId){
 
         try{
             Cart cart = cartService.getCart(cartId);
-            return ResponseEntity.ok(new ApiResponse("Cart found successfully", cart));
+            return ResponseEntity.ok().body(cart);
         }catch (ResourceNotFoundException e){
-            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+            return ResponseEntity.badRequest().body(null);
         }
     }
 
-    @DeleteMapping("delete/{cartId}")
-    public ResponseEntity<ApiResponse> clearCart(@PathVariable Long cartId){
+    @DeleteMapping("delete")
+    public void clearCart(@RequestParam Long cartId){
 
         try{
+            System.out.println("Clear Cart Triggered");
             cartService.clearCart(cartId);
-            return ResponseEntity.ok(new ApiResponse("Cart cleared successfully", null));
         }catch (ResourceNotFoundException e){
-            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+            System.out.println(e.getMessage());
         }
     }
 
